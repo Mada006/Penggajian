250487c8a1c24815c70edaeda02be015c95c1f7c
Muhammad Aqida Putra Amada <129835316+Mada006@users.noreply.github.com>
2023-04-04 22:36:45 +0700

    Add files via upload

---
+++ tugasPenggajian.py
@@ -0,0 +1,109 @@
+class Employee:
+
+    def __init__(self):
+
+        self.name = input ("Masukan Nama: ")
+
+        self.salary = int(input("Jumlah Gaji: "))
+
+        self.grade = int(input("Grade: "))
+
+        self.num_children = int(input("Jumlah Anak: "))
+
+        self.married = input("Menikah? ")
+
+    def get_salary(self):
+
+        # Hitung tunjangan golongan
+
+        if self.grade == 1:
+
+            allowance_grade = 0.05 * self.salary
+
+        elif self.grade == 2:
+
+            allowance_grade = 0.1 * self.salary
+        
+        elif self.grade == 3:
+
+            allowance_grade = 0.15 * self.salary
+        
+        elif self.grade == 4:
+
+            allowance_grade = 0.2 * self.salary
+
+        else:
+
+            allowance_grade = 0.15 * self.salary
+        
+        # Hitung tunjangan anak
+
+        if self.num_children > 0:
+
+            allowance_children = 0.02 * self.salary * self.num_children
+
+        else:
+
+            allowance_children = 0
+        
+        # Hitung tunjangan istri
+
+        if self.married == "ya":
+
+            allowance_spouse = 0.1 * self.salary
+
+        else:
+
+            allowance_spouse = 0
+        
+        # Hitung total gaji sebelum pajak
+
+        total_salary = self.salary + allowance_grade + allowance_children + allowance_spouse
+
+        # Hitung pajak
+
+        if total_salary <= 5000000:
+
+            tax = 0.05 * total_salary
+
+        elif total_salary <= 10000000:
+
+            tax = 0.1 * total_salary
+
+        else:
+
+            tax = 0.15 * total_salary
+
+        # Hitung bonus
+
+        if self.grade == 1 and self.num_children >= 3:
+
+            bonus = 300000
+        
+        if self.grade == 2 and self.num_children >= 3:
+
+            bonus = 400000
+        
+        elif self.grade == 3 and self.num_children >= 3:
+
+            bonus = 500000
+
+        else:
+
+            bonus = 0
+
+        # Hitung total gaji setelah pajak dan bonus
+
+        totall = total_salary - tax + bonus  
+    
+        '''print (allowance_grade)
+        print (allowance_children)
+        print (allowance_spouse)
+        print (total_salary)
+        print (tax)
+        print (bonus)'''
+        print ("Total Gaji: ",totall)
+
+
+a = Employee()
+a.get_salary()
